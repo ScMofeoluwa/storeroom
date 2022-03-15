bd7d0e07b15541a2c982ca8dfc211dcf0395e9f5
Mofeoluwa Lijadu <lijsamobafunmilayo@gmail.com>
2022-03-15 23:30:07 +0100

    feat: signin route

---
+++ routes/signin.js
@@ -0,0 +1,31 @@
+const { User } = require("../models");
+const Joi = require("joi");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const express = require("express");
+const router = express.Router();
+
+router.post("/", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const user = await User.findOne({ where: { email: req.body.email } });
+  if (!user) return res.status(400).send("Invalid email or password");
+
+  const validPassword = await bcrypt.compare(req.body.password, user.password);
+  if (!validPassword) return res.status(400).send("Invalid email or password");
+
+  const token = jwt.sign({ id: user.id }, process.env.SECRET_KEY);
+  res.send(token);
+});
+
+function validate(req) {
+  const schema = Joi.object({
+    email: Joi.string().min(5).max(255).required().email(),
+    password: Joi.string().min(5).max(255).required(),
+  });
+
+  return schema.validate(req);
+}
+
+module.exports = router;
