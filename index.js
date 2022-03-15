ef1b78c58f6943b9d36baa107d62b0d9bbc0acd2
Mofeoluwa Lijadu <lijsamobafunmilayo@gmail.com>
2022-03-15 23:31:35 +0100

    chore: lezzgedizz

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require("express");
+const error = require("./middleware/error");
 
-const users = require("./routes/user");
+const signup = require("./routes/signup");
+const signin = require("./routes/signin");
 
 const app = express();
 const port = process.env.PORT;
 
 app.use(express.json());
-app.use("/api/users", users);
+app.use("/api/signup", signup);
+app.use("/api/signin", signin);
+
+app.use(error);
 
 app.listen(port, () => {
   console.log(`Listening on port: ${port}`);
