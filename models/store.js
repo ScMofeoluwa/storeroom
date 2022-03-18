7a0b6974b791167563dd0511ce1af19ca61e712e
Mofeoluwa Lijadu <lijsamobafunmilayo@gmail.com>
2022-03-18 23:42:49 +0100

    refactor: store model

--- models/store.js
+++ models/store.js
@@ -2,11 +2,6 @@
 const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Store extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
       this.belongsTo(models.User, {
         foreignKey: "userId",
