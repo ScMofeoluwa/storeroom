6c7659f4fd125ee540927cc5a6954943e7ee598e
Mofeoluwa Lijadu <lijsamobafunmilayo@gmail.com>
2022-03-24 15:14:19 +0100

    feat: verify

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const error = require("./middleware/error");
 const register = require("./routes/register");
 const login = require("./routes/login");
 const stores = require("./routes/store");
+const verify = require("./routes/verifyAccount")
 
 const app = express();
 const port = process.env.PORT;
@@ -11,6 +12,7 @@ const port = process.env.PORT;
 app.use(express.json());
 app.use("/api/register", register);
 app.use("/api/login", login);
+app.use("/api/verify", verify)
 app.use("/api/stores", stores);
 
 app.use(error);
