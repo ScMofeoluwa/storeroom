aa258c35b5d983c5e03d30d2cda0b4e12c60aa99
Mofeoluwa Lijadu <lijsamobafunmilayo@gmail.com>
2022-03-26 06:06:34 +0100

    feat: product model

---
+++ models/product.js
@@ -0,0 +1,61 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Product extends Model {
+    static associate(models) {
+      this.belongsTo(models.Store, {
+        foreignKey: "storeId",
+        as: "store",
+        onDelete: "CASCADE",
+      });
+    }
+  }
+  Product.init(
+    {
+      name: {
+        type: DataTypes.STRING,
+        validate: {
+          len: {
+            args: [5, 20],
+            msg: "name must be between 5 and 20 characters",
+          },
+        },
+        allowNull: false,
+      },
+      price: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+          min: {
+            args: 0,
+            msg: "minimum price must be 0",
+          },
+        },
+      },
+      quantity: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+          min: {
+            args: 0,
+            msg: "minimum price must be 0",
+          },
+        },
+      },
+      storeId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: "store ID cannot be empty",
+          },
+        },
+      },
+    },
+    {
+      sequelize,
+      modelName: "Product",
+    }
+  );
+  return Product;
+};
