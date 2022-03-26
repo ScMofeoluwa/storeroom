e9c259f0a2161ac7772fc8405567c1847ae2a27e
Mofeoluwa Lijadu <lijsamobafunmilayo@gmail.com>
2022-03-26 07:26:49 +0100

    refactor: register route

--- routes/register.js
+++ routes/register.js
@@ -6,19 +6,30 @@ const express = require("express");
 const router = express.Router();
 
 router.post("/", async (req, res) => {
-  let user = await User.findOne({ where: { email: req.body.email } });
-  if (user) return res.status(400).send("User already registered");
+  try {
+    let user = await User.findOne({ where: { email: req.body.email } });
+    if (user) return res.status(400).send("User already registered");
 
-  const salt = await bcrypt.genSalt(10);
-  req.body.password = await bcrypt.hash(req.body.password, salt);
+    const salt = await bcrypt.genSalt(10);
+    req.body.password = await bcrypt.hash(req.body.password, salt);
 
-  user = await User.create(_.pick(req.body, ["username", "email", "password"]));
+    user = await User.create(
+      _.pick(req.body, ["username", "email", "password"])
+    );
 
-  const verificationToken = user.generateVerificationToken();
-  sendMail(user, verificationToken);
-  return res.status(200).send({
-    message: `Verification mail was sent to ${user.email}`,
-  });
+    const verificationToken = user.generateVerificationToken();
+    sendMail(user, verificationToken);
+    return res.status(200).send({
+      message: `Verification mail was sent to ${user.email}`,
+    });
+  } catch (err) {
+    if (err.name === "SequelizeValidationError") {
+      return res.status(400).json({
+        success: false,
+        msg: err.errors.map((e) => e.message),
+      });
+    }
+  }
 });
 
 module.exports = router;
