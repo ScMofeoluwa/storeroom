e875d74f3bd6f3b0f3fb7c6113f96924b76d6861
Mofeoluwa Lijadu <lijsamobafunmilayo@gmail.com>
2022-03-28 03:21:48 +0100

    feat: product route

---
+++ routes/product.js
@@ -0,0 +1,83 @@
+const { Store, Product } = require("../models");
+const auth = require("../middleware/auth");
+const _ = require("lodash");
+const express = require("express");
+const router = express.Router({ mergeParams: true });
+
+router.get("/:productId", auth, async (req, res) => {
+  const store = await Store.findByPk(req.params.storeId);
+  if (!store || store.userId !== req.user.id)
+    return res
+      .status(404)
+      .send({ message: "Store with the given ID doesn't exist" });
+  const product = await Product.findByPk(req.params.productId);
+  if (!product || product.storeId !== parseInt(req.params.storeId))
+    return res
+      .status(404)
+      .send({ message: "Product with given ID doesn't exist" });
+
+  res.send(product);
+});
+
+router.post("/", auth, async (req, res) => {
+  try {
+    const store = await Store.findByPk(req.params.storeId);
+    if (!store || store.userId !== req.user.id)
+      return res
+        .status(404)
+        .send({ message: "Store with the given ID doesn't exist" });
+    const newProduct = _.merge(req.body, { storeId: req.params.storeId });
+    const product = await Product.create(newProduct);
+    res.status(201).send(product);
+  } catch (err) {
+    if (err.name === "SequelizeValidationError") {
+      return res.status(400).json({
+        success: false,
+        msg: err.errors.map((e) => e.message),
+      });
+    }
+  }
+});
+
+router.put("/:productId", auth, async (req, res) => {
+  try {
+    const store = await Store.findByPk(req.params.storeId);
+    if (!store || store.userId !== req.user.id)
+      return res
+        .status(404)
+        .send({ message: "Store with the given ID doesn't exist" });
+    let product = await Product.findByPk(req.params.productId);
+    if (!product || product.storeId !== parseInt(req.params.storeId))
+      return res
+        .status(404)
+        .send({ message: "Product with given ID doesn't exist" });
+    product = await product.set(req.body);
+    console.log("got here");
+    await product.save();
+    res.send(product);
+  } catch (err) {
+    if (err.name === "SequelizeValidationError") {
+      return res.status(400).json({
+        success: false,
+        msg: err.errors.map((e) => e.message),
+      });
+    }
+  }
+});
+
+router.delete("/:productId", auth, async (req, res) => {
+  const store = await Store.findByPk(req.params.storeId);
+  if (!store || store.userId !== req.user.id)
+    return res
+      .status(404)
+      .send({ message: "Store with the given ID doesn't exist" });
+  const product = await Product.findByPk(req.params.productId);
+  if (!product || product.storeId !== parseInt(req.params.storeId))
+    return res
+      .status(404)
+      .send({ message: "Product with given ID doesn't exist" });
+  await product.destroy();
+  res.send(product);
+});
+
+module.exports = router;
