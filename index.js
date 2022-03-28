c6642e469c8459971a145415b8a33d7033148bfb
Mofeoluwa Lijadu <lijsamobafunmilayo@gmail.com>
2022-03-28 03:22:54 +0100

    feat: product

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const error = require("./middleware/error");
 const register = require("./routes/register");
 const login = require("./routes/login");
 const stores = require("./routes/store");
-const verify = require("./routes/verifyAccount")
+const verify = require("./routes/verifyAccount");
+const products = require("./routes/product");
 
 const app = express();
 const port = process.env.PORT;
@@ -12,8 +13,9 @@ const port = process.env.PORT;
 app.use(express.json());
 app.use("/api/register", register);
 app.use("/api/login", login);
-app.use("/api/verify", verify)
+app.use("/api/verify", verify);
 app.use("/api/stores", stores);
+app.use("/api/:storeId/products", products);
 
 app.use(error);
 
