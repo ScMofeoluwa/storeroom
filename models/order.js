d7dfd66012c2f4d4be7a65d21b914588c286240a
Mofeoluwa Lijadu <lijsamobafunmilayo@gmail.com>
2022-04-02 09:39:00 +0100

    feat: order model

---
+++ models/order.js
@@ -0,0 +1,53 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Order extends Model {
+    static associate(models) {
+      this.belongsToMany(models.Product, {
+        through: "ProductOrder",
+        as: "products",
+        foreignKey: "orderId",
+        otherKey: "productId",
+      });
+    }
+  }
+  Order.init(
+    {
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          isEmail: {
+            msg: "Email must be a valid one",
+          },
+        },
+      },
+      firstName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      lastName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      phoneNumber: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      txnRef: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      status: {
+        type: DataTypes.STRING,
+        defaultValue: "pending",
+        values: ["pending", "success", "failed"],
+      },
+    },
+    {
+      sequelize,
+      modelName: "Order",
+    }
+  );
+  return Order;
+};
