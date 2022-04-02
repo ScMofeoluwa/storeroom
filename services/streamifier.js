69b34909fcc259e0086161b8797d764041d1c547
Mofeoluwa Lijadu <lijsamobafunmilayo@gmail.com>
2022-04-02 13:17:48 +0100

    feat: cloudinary service

---
+++ services/streamifier.js
@@ -0,0 +1,26 @@
+const streamifier = require("streamifier");
+const cloudinary = require("cloudinary").v2;
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const fileUpload = (req) => {
+  return new Promise((resolve, reject) => {
+    let stream = cloudinary.uploader.upload_stream(
+      { folder: "images" },
+      (error, result) => {
+        if (result) {
+          resolve(result);
+        } else {
+          reject(error);
+        }
+      }
+    );
+    streamifier.createReadStream(req.file.buffer).pipe(stream);
+  });
+};
+
+exports.fileUpload = fileUpload;
