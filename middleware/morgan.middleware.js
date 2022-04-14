87c714b07d8bdb7e074cca65cf86a8d3bd76bcd8
Mofeoluwa Lijadu <lijsamobafunmilayo@gmail.com>
2022-04-14 18:19:33 +0100

    feat: morgan middleware

---
+++ middleware/morgan.middleware.js
@@ -0,0 +1,18 @@
+const logger = require("../utils/logger");
+const morgan = require("morgan");
+
+const stream = {
+  write: (message) => logger.http(message),
+};
+
+const skip = () => {
+  const env = process.env.NODE_ENV || "development";
+  return env !== "development";
+};
+
+const morganMiddleware = morgan(
+  ":method :url :status :res[content-length] - :response-time ms",
+  { stream, skip }
+);
+
+module.exports = morganMiddleware;
