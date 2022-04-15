a394d1857d062d987c48c89435d3894a6ddd580c
Mofeoluwa Lijadu <lijsamobafunmilayo@gmail.com>
2022-04-15 13:15:57 +0100

    feat: refresh token route

---
+++ routes/refresh.js
@@ -0,0 +1,12 @@
+const { User } = require("../models");
+const auth = require("../middleware/auth");
+const express = require("express");
+const router = express.Router();
+
+router.post("/", auth, async (req, res) => {
+  const user = await User.findByPk(req.user.id);
+  const accessToken = user.generateAccessToken();
+  return res.send({ accessToken: accessToken });
+});
+
+module.exports = router;
