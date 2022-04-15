4b68c09b5d990ece78317e77028658d2e0ddc877
Mofeoluwa Lijadu <lijsamobafunmilayo@gmail.com>
2022-04-15 13:13:32 +0100

    refactor: auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,14 +1,19 @@
+const jwt = require("jsonwebtoken");
+
 const env = process.env.NODE_ENV || "development";
 const config = require("../config/config.js")[env];
-const jwt = require("jsonwebtoken");
 
 module.exports = function (req, res, next) {
   const header = req.header("Authorization");
   const bearer = header.split(" ");
   const token = bearer[1];
+  const url = req.originalUrl.split("/").at(-1);
+  const isRefresh = url === "refresh";
   if (!token) return res.status(401).send("Access Denied: No token provided");
   try {
-    const payload = jwt.verify(token, config.atSecret);
+    const payload = isRefresh
+      ? jwt.verify(token, config.rtSecret)
+      : jwt.verify(token, config.secret);
     req.user = payload;
     next();
   } catch (ex) {
