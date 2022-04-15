d0dababb447ff19ff5f2397730cfc89a8c3b3854
Mofeoluwa Lijadu <lijsamobafunmilayo@gmail.com>
2022-04-15 13:17:38 +0100

    feat: refresh

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const logger = require("./utils/logger");
 const morganMiddleware = require("./middleware/morgan.middleware");
 
 const register = require("./routes/register");
-const login = require("./routes/login");
-const stores = require("./routes/store");
 const verify = require("./routes/verifyAccount");
+const login = require("./routes/login");
+const refresh = require("./routes/refresh");
+const stores = require("./routes/store");
 const products = require("./routes/product");
 const images = require("./routes/image");
 const orders = require("./routes/order");
@@ -15,8 +16,9 @@ const port = process.env.PORT;
 
 app.use(express.json());
 app.use("/api/register", register);
-app.use("/api/login", login);
 app.use("/api/verify", verify);
+app.use("/api/login", login);
+app.use("/api/refresh", refresh);
 app.use("/api/stores", stores);
 app.use("/api/:storeId/products", products);
 app.use("/api/:storeId/products/:productId/image", images);
